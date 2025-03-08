002bf11b0b6690ad323a7d214b2536d7cd4cd47c
Max <maxi.dmgz@gmail.com>
2025-03-08 23:26:06 +0100

    Eliminando un archivo que no tenia que estar.

--- Pruebas.py
+++
@@ -1,9 +0,0 @@
-from importlib.metadata import version, PackageNotFoundError
-
-dependencias = ["speechrecognition", "pyttsx3", "fuzzywuzzy", "num2words", "pycaw", "datetime", "os.path", "google.oauth2.credentials", "google_auth_oauthlib.flow", "googleapiclient.discovery", ""]
-
-for paquete in dependencias:
-    try:
-        print(f"{paquete}: {version(paquete)}")
-    except PackageNotFoundError:
-        print(f"{paquete} no está instalado")
